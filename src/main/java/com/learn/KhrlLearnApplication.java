ec86f6f4ad6c97da2ed0a86102ed18180e94d0d7
DESKTOP-R4J5FCV\Khairul <khairul.umam3@jd.id>
2023-01-15 12:57:26 +0700

    initial commit learn kafka

---
+++ src/main/java/com/learn/KhrlLearnApplication.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhrlLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhrlLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/KhrlLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhrlLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
